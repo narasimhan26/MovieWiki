6b1c64740ca7292a974ea83e572c5ac1c8978e8f
narasimhan26 <narasimhanaakash26@gmail.com>
2019-11-07 13:17:45 +0530

    Uploading files

---
+++ JS/index.js
@@ -0,0 +1,50 @@
+var movies = [];
+var page = 1;
+function myFunction() {
+    //document.getElementById("display").innerHTML = "";
+    var searchValue = document.getElementById("search").value;
+    searchValue = searchValue.trim().replace(' ','+');
+    
+    //getting search results from API 
+
+    var url = 'http://www.omdbapi.com/?apikey=79f0b98a&&s='+searchValue+'&page='+page;
+    //var url = "./sample.json";
+    console.log(url);
+    fetch(url)
+        .then(function(response) {
+        return response.json();
+        })
+        .then(function(myJson) {
+        
+        // appending serach results into html
+
+        var search = myJson['Search'];
+        if(search!=null){
+            movies = [...movies,...search];
+            var htmls = [];
+            search.forEach((json, index) => {
+            var html = "<div class='card' onclick=displayMovie('"+json['imdbID']+"')><img src=" + json['Poster']+" id='poster' style='width:100%'><div class='container'><h4><b>"+json['Title']+"</b></h4><p id='year'>"+json['Year']+"</p></div></div>";
+            htmls.push(html);
+            document.getElementById("display").innerHTML = htmls.join("\n");
+            });
+
+            // pagination
+
+            if(movies.length!=myJson.totalResults){
+                page++;
+                document.getElementById("loadMore").classList.remove("invisible"); 
+            }else{
+                page++;
+                document.getElementById("loadMore").classList.add("invisible"); 
+            }
+        }
+        //console.log(htmls.length);        
+        });
+}
+
+//moving to next page using movie id
+
+function displayMovie(id){
+    window.location.href = "movie.html?id="+id;
+    console.log(history.forward());
+}
---
+++ JS/movie.js
@@ -0,0 +1,44 @@
+function getMovieDetails(){
+    var id = window.location.search.split('?id=')[1];
+    var url = 'http://www.omdbapi.com/?apikey=79f0b98a&&i='+id; // geting details of the movie using Movie id
+    fetch(url)
+    .then(function(response) {
+     return response.json();
+    })
+    .then(function(myJson) {
+    console.log(myJson);
+    displayDetails(myJson);
+    });
+}
+
+function goBack(){
+    window.history.back();
+}
+
+function displayDetails(myJson){
+
+    //displaying details to htmls using innerHTML
+
+    document.getElementById('movie-img').src = myJson['Poster'];
+    document.getElementById('imdb').innerText = "IMDb: "+ myJson['imdbRating'];
+    document.getElementById('movie-title').innerHTML = myJson["Title"];
+    document.getElementById('movie-year').innerHTML = myJson['Year']+" | "+myJson['Runtime']+" | "+myJson['Released'];
+    document.getElementById('plot').innerHTML = myJson["Plot"];
+    document.getElementById('directors').innerHTML = "<b>Director(s): </b>"+myJson['Director'];
+    document.getElementById('writers').innerHTML = "<b>Writer(s): </b>" + myJson['Writer'];
+    let genres = myJson['Genre'].split(", ");
+    let htmls = [];
+    genres.forEach(element => {
+        var html = "<span class='genre'>"+element+"</span>";
+        htmls.push(html);
+    });
+    //console.log(htmls);
+    document.getElementById("genre-list").innerHTML = htmls.join("\n");
+    htmls = [];
+    let casts = myJson['Actors'].split(", ");
+    casts.forEach(element =>{
+        var html = "<li>"+element+"</li>";
+        htmls.push(html);
+    });
+    document.getElementById("casts").innerHTML = htmls.join("\n");
+}
